21e81774bcee398ee9894105a5f2503083d4dbfe
arektar <zander5@mail.ru>
2017-01-21 18:41:33 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -15,16 +15,16 @@ def get_password_strength(password):
     good_pass_len = 6
     not_only_low_reg = bool(password.lower() != password)
     not_only_upp_reg = bool(password.upper() != password)
-    include_numbers = re.search("[1-9]", password).group(0) is not None
-    not_phone_number = re.search(".?\d.?.?\d{3}.?.?\d{3}.?\d{2}.?\d{2}", password).group(0) is None
-    not_date = re.search("[0-3]\d.?[0-1]\d.?(\d{4}|\d{2})", password).group(0) is None
-    not_car_number = re.search("[а-я]\d{3}[а-я]{2}(\d{2,3})?", password).group(0) is None
-    include_special_sim = re.search("\W", password).group(0) is not None
+    include_numbers = re.search("[1-9]", password) is not None
+    not_phone_number = re.search(".?\d.?.?\d{3}.?.?\d{3}.?\d{2}.?\d{2}", password) is None
+    not_date = re.search("[0-3]\d.?[0-1]\d.?(\d{4}|\d{2})", password) is None
+    not_car_number = re.search("[а-я]\d{3}[а-я]{2}(\d{2,3})?", password) is None
+    include_special_sim = re.search("\W", password) is not None
     good_len = len(password) > good_pass_len
     great_len = len(password) > (good_pass_len * 2)
     not_in_blacklist = check_in_blacklist(password) is False
-    return sum(not_only_low_reg, not_only_upp_reg, include_numbers, not_phone_number, not_date, not_car_number,
-               include_special_sim, good_len, great_len, not_in_blacklist)
+    return sum([not_only_low_reg, not_only_upp_reg, include_numbers, not_phone_number, not_date, not_car_number,
+               include_special_sim, good_len, great_len, not_in_blacklist])
 
 
 if __name__ == '__main__':
