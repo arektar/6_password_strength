681d9453db88219220b33e1c0e0a039d61e52c8d
arektar <zander5@mail.ru>
2017-03-03 16:08:09 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -8,31 +8,32 @@ def take_blacklist(blacklist_file_path):
     return black_list_text
 
 
-def prepare_blacklist(blacklist_file_path=r'C:\password_blacklist'):
+def prepare_blacklist(blacklist_file_path):
     black_list_text = take_blacklist(blacklist_file_path)
     blacklist = black_list_text.split('\n')
     return blacklist
 
 
-def get_password_strength(password):
+def get_password_strength(password, blacklist_file_path):
     good_pass_len = 6
-    password_not_only_low_register = bool(password.lower() != password)
-    password_not_only_upp_register = bool(password.upper() != password)
-    password_include_numbers = re.search("[1-9]", password) is not None
-    password_has_no_phone_number = re.search(".?\d.?.?\d{3}.?.?\d{3}.?\d{2}.?\d{2}", password) is None
-    password_has_no_date = re.search("[0-3]\d.?[0-1]\d.?(\d{4}|\d{2})", password) is None
-    password_has_no_car_number = re.search("[а-я]\d{3}[а-я]{2}(\d{2,3})?", password) is None
-    password_include_special_characters = re.search("\W", password) is not None
-    password_have_good_len = len(password) > good_pass_len
-    password_have_great_len = len(password) > (good_pass_len * 2)
-    password_not_in_blacklist = password not in prepare_blacklist()
+    not_only_low_register = password.lower() != password
+    not_only_upp_register = password.upper() != password
+    include_numbers = re.search("[1-9]", password) is not None
+    no_phone_number_found = re.search(".?\d.?.?\d{3}.?.?\d{3}.?\d{2}.?\d{2}", password) is None
+    no_date_found = re.search("[0-3]\d.?[0-1]\d.?(\d{4}|\d{2})", password) is None
+    no_car_number_found = re.search("[а-я]\d{3}[а-я]{2}(\d{2,3})?", password) is None
+    include_special_characters = re.search("\W", password) is not None
+    good_len = len(password) > good_pass_len
+    great_len = len(password) > (good_pass_len * 2)
+    no_stop_words = password not in prepare_blacklist(blacklist_file_path)
     return sum(
-        [password_not_only_low_register, password_not_only_upp_register, password_include_numbers,
-         password_has_no_phone_number, password_has_no_date, password_has_no_car_number,
-         password_include_special_characters, password_have_good_len, password_have_great_len,
-         password_not_in_blacklist])
+        [not_only_low_register, not_only_upp_register, include_numbers,
+         no_phone_number_found, no_date_found, no_car_number_found,
+         include_special_characters, good_len, great_len,
+         no_stop_words])
 
 
 if __name__ == '__main__':
+    blacklist_path = input("Введите путь к файлу исключений: ")
     password_for_assessment = getpass.getpass()
-    print("Ваш пароль получает %d баллов из 10" % (get_password_strength(password_for_assessment)))
+    print("Ваш пароль получает %d баллов из 10" % (get_password_strength(password_for_assessment, blacklist_path)))
